4246a4c88731ef9471330507f5f1abc229f63010
auau1662fr <auric.chen.2005@gmail>
2026-01-08 12:12:59 +0100

    Implement basic shell with builtins and signal handling

--- exec.c
+++ exec.c
@@ -9,26 +9,43 @@
 
 int execute_cmd(char **argv)
 {
+	char *cmd_path;
 	pid_t pid;
-	char *path;
+	int status;
 
-	path = find_path(argv[0]);
-	if (!path)
+	/* Cherche la commande dans le PATH */
+	cmd_path = find_path(argv[0]);
+	if (!cmd_path)
 	{
-		fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
-		return (1);
+/* Commande introuvable, affiche erreur comme ./hsh: 1: cmd: not found */
+	fprintf(stderr, "%s: 1: %s: not found\n", "./hsh", argv[0]);
+	return (1);
 	}
 
+/* Fork uniquement si la commande existe */
 	pid = fork();
-	if (pid == 0)
+	if (pid == -1)
 	{
-		execve(path, argv, environ);
-		perror("execve");
-		exit(1);
+	perror("fork");
+	free(cmd_path);
+	return (1);
 	}
-	else if (pid > 0)
-		wait(NULL);
 
+	if (pid == 0) /* Processus enfant */
+	{
+	if (execve(cmd_path, argv, environ) == -1)
+		{
+		perror("execve");
+		free(cmd_path);
+		exit(EXIT_FAILURE);
+		}
+	}
+	else /* Parent attend */
+	{
+	waitpid(pid, &status, 0);
+	}
+
+	free(cmd_path);
 	return (0);
 }
 
