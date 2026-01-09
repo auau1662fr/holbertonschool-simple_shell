3bc249851f67c1d43fecdb7e1a2a69d4e326a4a7
auau1662fr <auric.chen.2005@gmail.com>
2026-01-09 17:24:21 +0100

    Fix task 4: prevent fork when command is not found

--- exec.c
+++ exec.c
@@ -4,7 +4,7 @@
  * execute_cmd - executes a command
  * @argv: array of arguments
  *
- * Return: 0 on success, 1 on failure
+ * Return: 1 on failure, 0 on success
  */
 int execute_cmd(char **argv)
 {
@@ -13,13 +13,16 @@ int execute_cmd(char **argv)
 	int status;
 
 	path = find_path(argv[0]);
-	if (!path)
+	if (!path || access(path, X_OK) != 0)
 	{
 		fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
 		return (1);
 	}
 
 	pid = fork();
+	if (pid == -1)
+		return (1);
+
 	if (pid == 0)
 	{
 		if (execve(path, argv, environ) == -1)
